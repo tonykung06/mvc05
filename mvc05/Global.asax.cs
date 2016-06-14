c14dc4d0bc6a8a41c2b1fc8bd5bba1e19d1a1073
tonykung <tonykung06@gmail.com>
2016-06-14 23:32:51 +0800

    add custom display mode, so that auto file lookup like Index.iPhone.cshtml could be done

--- mvc05/Global.asax.cs
+++ mvc05/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace mvc05
 {
@@ -17,6 +18,22 @@ namespace mvc05
             //could also be configured in Web.config
             //HtmlHelper.ClientValidationEnabled = false;
             //HtmlHelper.UnobtrusiveJavaScriptEnabled = false;
+
+            //register custom display mode for different mobile browers
+            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("WP")
+            {
+                ContextCondition = (context => context.GetOverriddenUserAgent().IndexOf("Windows Phone OS", StringComparison.OrdinalIgnoreCase) >= 0)
+            });
+
+            DisplayModeProvider.Instance.Modes.Insert(1, new DefaultDisplayMode("iPhone")
+            {
+                ContextCondition = (context => context.GetOverriddenUserAgent().IndexOf("iPhone", StringComparison.OrdinalIgnoreCase) >= 0)
+            });
+
+            DisplayModeProvider.Instance.Modes.Insert(2, new DefaultDisplayMode("Android")
+            {
+                ContextCondition = (context => context.GetOverriddenUserAgent().IndexOf("Android", StringComparison.OrdinalIgnoreCase) >= 0)
+            });
         }
     }
 }
